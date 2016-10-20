e7d206d5c1ea29b7b659b143b1b7f1cf07487b95
Kang Mus <Kang Mus@KangMus-PC>
2016-10-20 19:12:15 +0700

    Ngebuat file baru di GitHub.

---
+++ NyobaGit/src/nyobagit/NyobaGit.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nyobagit;
+
+/**
+ *
+ * @author Kang Mus
+ */
+public class NyobaGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Print nyoba
+        System.out.println("Gua lagi nyoba, nih. :-D");
+    }
+    
+}
